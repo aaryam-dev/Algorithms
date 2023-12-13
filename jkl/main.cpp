7678b68bba299abcfa2420f95944429acb06293b
Aaryam Dev <aaryam.dev@gmail.com>
2023-12-13 16:44:18 +0530

    Initial Commit

---
+++ jkl/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  jkl
+//
+//  Created by Aaryam Dev on 13/12/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
